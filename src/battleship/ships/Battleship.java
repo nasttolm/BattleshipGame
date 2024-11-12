6ce35266b851eca595cff3935d4b5196732e2b47
nasttolm <nasttolm@gmail.com>
2024-11-12 09:22:40 +0000

    Implement ship inheritance by creating specialized ship classes (Battleship, Destroyer, Submarine) and Fleet management system.

---
+++ src/battleship/ships/Battleship.java
@@ -0,0 +1,9 @@
+package battleship.ships;
+
+import battleship.Ship;
+
+public class Battleship extends SimpleShip {
+    public Battleship() {
+        super("Battleship", "B", 5);
+    }
+}
---
+++ src/battleship/ships/Submarine.java
@@ -0,0 +1,9 @@
+package battleship.ships;
+
+import battleship.Ship;
+
+public class Submarine extends SimpleShip {
+    public Submarine() {
+        super("Submarine", "S", 3);
+    }
+}
